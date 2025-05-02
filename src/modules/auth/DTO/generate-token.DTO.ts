d726c6bbf7eaf724f1bcfd06dc2575a4d4bb3c07
erfan <erfan.wtf44@gmail.com>
2025-05-02 01:09:50 -0400

    chore: Adding GenerateToken Test"

--- src/modules/auth/DTO/generate-token.DTO.ts
+++ src/modules/auth/DTO/generate-token.DTO.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsJWT, IsNotEmpty } from "class-validator";
 
 export class GenerateTokenDTO {
+    @ApiProperty({ description: "user refresh Token" })
     @IsJWT()
     @IsNotEmpty()
     refreshToken: string
--- src/modules/auth/test/auth.service.spec.ts
+++ src/modules/auth/test/auth.service.spec.ts
@@ -4,10 +4,12 @@ import { HashingService } from "../hashing/hashing.abstract"
 import { hashServiceMock } from "./mock/hashingService.mock"
 import { UserService } from "../../user/user.service"
 import { RegisterUserDTO } from "../DTO/register-user.dto"
-import { NotFoundException } from "@nestjs/common"
+import { BadRequestException, NotFoundException } from "@nestjs/common"
 import { ErrorMessages } from "../../../errorResponses/errorResponse.enum "
 import { UserTokenService } from "../jwt-strategies/user-token.service"
 import { UserDocument, UserRole } from "../../../schemas"
+import { JsonWebTokenError } from "@nestjs/jwt"
+import { Types } from "mongoose"
 
 describe("AuthService", function () {
 
@@ -15,12 +17,16 @@ describe("AuthService", function () {
     let hashService: HashingService
 
     let mockTokenService = {
-        generateToken: jest.fn()
+        generateToken: jest.fn(),
+        verifyRefreshToken: jest.fn(),
+        isValidate: jest.fn(),
+        invalidate: jest.fn()
     } as unknown as UserTokenService;
 
     let userServiceMock = {
         createUser: jest.fn(),
-        findUserByEmail: jest.fn()
+        findUserByEmail: jest.fn(),
+        findUserById: jest.fn()
     }
 
     beforeEach(async () => {
@@ -143,6 +149,91 @@ describe("AuthService", function () {
                 name: "test-name",
                 role: UserRole.USER
             })
+        })
+    })
+
+
+    describe("Generate Token", () => {
+        it("Should Be Throw BadRequest Because JWT is expired.", async () => {
+            jest.spyOn(mockTokenService, 'verifyRefreshToken').mockRejectedValueOnce(new JsonWebTokenError("error"))
+
+
+            try {
+
+                await service.generateToken({ refreshToken: "invalid-token" });
+            } catch (err) {
+                expect(err).toBeInstanceOf(BadRequestException)
+                expect(err.message).toEqual(ErrorMessages.INVALID_REFRESH_TOKEN)
+                expect(mockTokenService.verifyRefreshToken).toHaveBeenCalledWith('invalid-token')
+            }
+        })
+
+
+        it("Should be Throw BadRequest Because Token Is Invalid", async () => {
+            jest.spyOn(mockTokenService, 'verifyRefreshToken').mockResolvedValueOnce({ tokenId: "test-tokenId", userId: "test-user-id" });
+            jest.spyOn(mockTokenService, 'isValidate').mockResolvedValueOnce(false);
+
+            try {
+                await service.generateToken({ refreshToken: "invalid-token" });
+            } catch (err) {
+                expect(err).toBeInstanceOf(BadRequestException)
+                expect(err.message).toEqual(ErrorMessages.INVALID_REFRESH_TOKEN)
+                expect(mockTokenService.verifyRefreshToken).toHaveBeenCalledWith('invalid-token')
+                expect(mockTokenService.isValidate).toHaveBeenCalledWith('test-tokenId', 'invalid-token');
+            }
+        })
+
+
+        it("Should be Throw BadRequest Because User Not found", async () => {
+            const objId = new Types.ObjectId()
+            jest.spyOn(mockTokenService, 'verifyRefreshToken').mockResolvedValueOnce({ tokenId: "test-tokenId", userId: objId.toString() });
+            jest.spyOn(mockTokenService, 'isValidate').mockResolvedValueOnce(true);
+            jest.spyOn(mockTokenService, 'invalidate').mockResolvedValueOnce();
+            jest.spyOn(userServiceMock, 'findUserById').mockResolvedValueOnce(null)
+
+            try {
+                await service.generateToken({ refreshToken: "valid-token" });
+            } catch (err) {
+                expect(err).toBeInstanceOf(BadRequestException)
+                expect(err.message).toEqual(ErrorMessages.USER_NOT_FOUND)
+                expect(mockTokenService.verifyRefreshToken).toHaveBeenCalledWith('valid-token')
+                expect(mockTokenService.isValidate).toHaveBeenCalledWith('test-tokenId', 'valid-token');
+                expect(mockTokenService.invalidate).toHaveBeenCalledWith('test-tokenId');
+                expect(userServiceMock.findUserById).toHaveBeenCalledWith(objId)
+            }
+        })
+
+
+        it("Should be Generate new Token Successfully", async () => {
+            const objId = new Types.ObjectId()
+            const fakeUserInput = {
+                email: "test-mail",
+                name: 'test-name',
+                id: objId,
+                role: UserRole.USER
+            }
+            jest.spyOn(mockTokenService, 'verifyRefreshToken').mockResolvedValueOnce({ tokenId: "test-tokenId", userId: objId.toString() });
+            jest.spyOn(mockTokenService, 'isValidate').mockResolvedValueOnce(true);
+            jest.spyOn(mockTokenService, 'invalidate').mockResolvedValueOnce();
+            jest.spyOn(userServiceMock, 'findUserById').mockResolvedValueOnce(fakeUserInput)
+            jest.spyOn(mockTokenService, 'generateToken').mockResolvedValueOnce({ accessToken: 'test-access', refreshToken: 'test-refresh' })
+
+            const result = await service.generateToken({ refreshToken: "valid-token" });
+            expect(result.accessToken).toEqual('test-access')
+            expect(result.refreshToken).toEqual('test-refresh')
+            expect(mockTokenService.verifyRefreshToken).toHaveBeenCalledWith('valid-token')
+            expect(mockTokenService.isValidate).toHaveBeenCalledWith('test-tokenId', 'valid-token');
+            expect(mockTokenService.invalidate).toHaveBeenCalledWith('test-tokenId');
+            expect(userServiceMock.findUserById).toHaveBeenCalledWith(objId)
+            expect(mockTokenService.generateToken).toHaveBeenCalledWith(
+                {
+                    email: fakeUserInput.email,
+                    id: objId,
+                    name: fakeUserInput.name,
+                    role: fakeUserInput.role
+                }
+            )
+
         })
     })
 })
