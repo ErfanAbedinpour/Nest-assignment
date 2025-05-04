b9f49e4979d165533a9b5347fa41fa852da2a10d
erfan <erfan.wtf44@gmail.com>
2025-05-04 00:01:54 -0400

    chore: Updating .env.example

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,18 +2,17 @@ export interface IEnv {
   PORT: number;
   PREFIX: string;
   VERSION: string;
-  MONGO_URI: string
+  MONGO_URI: string;
   ACCESS_TOKEN_SECRET: string;
   ACCESS_TOKEN_EXPIRE: string;
   REFRESH_TOKEN_SECRET: string;
   REFRESH_TOKEN_EXPIRE: string;
   NODE_ENV: 'development' | 'production';
   ARGON_SECRET: string;
-  OPEN_ROUTER_API_KEY: string;
 }
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends IEnv { }
+    interface ProcessEnv extends IEnv {}
   }
 }
